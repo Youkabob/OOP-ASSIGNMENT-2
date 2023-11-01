96c1ff5fdcae479f173f4582fdf3cb98f2048b1d
YoussefEhab <114459225+Youkabob@users.noreply.github.com>
2023-11-01 20:52:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
-#include "Bigreal.h"
-#include "Bigreal.cpp"
+#include "BigReal.h"
+#include "BigReal.cpp"
 using namespace std;
 
 int main() {
-string s1,s2;
-cout<<"enter the first number: ";
-cin>>s1;
-    BigReal num1(s1);
-cout<<num1<<"\n";
-    cout<<"\nenter the second number: ";
-cin>>s2;
+    string s1,s2;
+    cout<<"enter the first number: ";
+    cin>>s1;
+    cin>>s2;
+    BigReal num1 (s1);
     BigReal num2(s2);
-    cout<<num2<<"\n";
 
+    if(num1==num2){
+        cout<<"THEY ARE EQUAL"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+if(num1<num2){
+    cout<<"Num2 is greater";
+}
+else{
+    cout<<"NO"<<endl;
+}
+if(num1>num2){
+    cout<<"Num1 is greater";
+}
+else{
+    cout<<"NO";
+}
 
 }
