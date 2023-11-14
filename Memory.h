26cefa100e4feabc1771895d2f9d779936791207
YoussefEhab <114459225+Youkabob@users.noreply.github.com>
2023-11-14 23:34:49 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -7,9 +7,14 @@ class Memory{
 private:
     string slots[256];
 public:
+    Memory(){
+        fill(slots,slots+256,"00");
+    }
     string getslot(string &address);
 
     void setslot(string &address,string value);
+
+    void displaymemory();
 
 };
 #endif //MACHINE_SIMULATOR_YOUKA_MEMORY_H
